afaf949a7925c784ec3fff990f9cadf84981c391
ansh mittal <anshmittal280706@gmail.com>
2026-07-30 19:46:21 +0530

    finished dashboard ui for supplier management

--- src/layouts/supplierManagement/SupplierManagemetnLayout.jsx
+++ src/layouts/supplierManagement/SupplierManagemetnLayout.jsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
-
-import Sidebar from "../components/supplierManagement/dashboard/Sidebar";
-import Topbar from "../components/supplierManagement/dashboard/Topbar";
-
+import Sidebar from "../../components/supplierManagement/dashboard/Sidebar"
+import Topbar from "../../components/supplierManagement/dashboard/Topbar";
 const SupplierManagemetnLayout = () => {
   return (
     <div className="flex min-h-screen bg-[#F8FCFF]">
       <Sidebar />
 
-      <div className="ml-[280px] flex flex-1 flex-col">
+      <div className="ml-70 flex flex-1 flex-col">
         <Topbar />
 
         <main className="flex-1 p-8">
